f0115706db543f90bb9a6a72e0bff1c5c86681f0
Sebastian <sebasortizsanta>
2025-04-20 19:33:31 -0300

    Sebas: [add] Importing and exporting components

--- 1ReactBasics/1firstapp/src/App.js
+++ 1ReactBasics/1firstapp/src/App.js
@@ -1,26 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import Promo from "./components/Promo";
+import Intro1 from "./components/Intro1";
+import Intro2 from "./components/Intro2";
+import Intro3 from "./components/Intro3";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <header className="App-header">
+              <p>
+                  Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Learn React
+              </a>
 
-        <Header/>
-        <Heading/>
-      </header>
+              <UsingAllComponents/>
+          </header>
+      </div>
+  );
+}
+
+function UsingAllComponents() {
+    return (
+        <div>
+        <ul>
+            <Nav/>
+            <Promo/>
+            <Intro1/>
+            <Intro2/>
+            <Intro3/>
+            <Footer/>
+        </ul>
     </div>
   );
 }
